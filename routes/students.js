4f848dfff41b4d2522d648a443ef6b455f901bf5
Allan Nielsen <allannielsen@Allans-MacBook.local>
2015-09-02 17:13:58 -0500

    initial commit - step 13 complet

---
+++ routes/students.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var students = require('../models/students.json');
+
+var file = path.join(__dirname, '../models/students.json');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log('in students get router', res) ;
+  //res.render(students);
+  res.send({"student":students});
+});
+
+router.post('/', function(req,res,next){
+
+  var studentsArray = students;
+  console.log(students);
+
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+
+  studentsArray.push({"firstName": req.body.firstName, "lastName": req.body.lastName});
+
+  studentsArray = JSON.stringify(studentsArray);
+  console.log(studentsArray);
+
+  fs.writeFile(file, studentsArray, 'utf8', function(err){
+    if(err){
+      console.log(err);
+      next(err);
+    }
+    else{
+      console.log('write success');
+      res.sendStatus(200);
+    }
+  })
+
+});
+
+
+module.exports = router;
